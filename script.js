b10676f071d44cff1e550404324b7d3bd6a6f9b4
Fardeen Pathan <167503530+fardeen2036@users.noreply.github.com>
2026-01-17 17:31:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const hero = document.getElementById("hero");
+const reveals = document.querySelectorAll(".reveal");
 
 window.addEventListener("scroll", () => {
+  // Hero morph
   if (window.scrollY > 50) {
     hero.classList.add("morphed");
   } else {
     hero.classList.remove("morphed");
   }
-});
 
+  // Section reveal
+  reveals.forEach(section => {
+    const top = section.getBoundingClientRect().top;
+    const trigger = window.innerHeight - 100;
+
+    if (top < trigger) {
+      section.classList.add("active");
+    }
+  });
+});
