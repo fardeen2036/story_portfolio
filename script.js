0655060c67e3792a76c028e647ada8d734a2b02c
Fardeen Pathan <167503530+fardeen2036@users.noreply.github.com>
2026-01-17 17:26:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const hero = document.getElementById("hero");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    hero.classList.add("morphed");
+  } else {
+    hero.classList.remove("morphed");
+  }
+});
 
